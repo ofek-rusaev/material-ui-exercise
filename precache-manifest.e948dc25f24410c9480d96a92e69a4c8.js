0994328d8c6c7b6daeab131b638ddb9ede2c2d83
ofek-rusaev <ofek2602@gmail.com>
2020-09-16 12:09:05 +0300

    Updates

---
+++ precache-manifest.e948dc25f24410c9480d96a92e69a4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e28865b2e374e296fe15389676526f",
+    "url": "/ofek-rusaev/material-ui-exercise/index.html"
+  },
+  {
+    "revision": "4028c7ef34f5482b5f6a",
+    "url": "/ofek-rusaev/material-ui-exercise/static/css/main.6c774188.chunk.css"
+  },
+  {
+    "revision": "bc2718f1a637d85f1aaf",
+    "url": "/ofek-rusaev/material-ui-exercise/static/js/2.59b0eca5.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/ofek-rusaev/material-ui-exercise/static/js/2.59b0eca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4028c7ef34f5482b5f6a",
+    "url": "/ofek-rusaev/material-ui-exercise/static/js/main.a2cf3dfa.chunk.js"
+  },
+  {
+    "revision": "4739d1bf4779e13d7e66",
+    "url": "/ofek-rusaev/material-ui-exercise/static/js/runtime-main.0cf5fc19.js"
+  }
+]);
